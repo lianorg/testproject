d42b63a38c8441868a065226fc62b9731367d545
liAnOfS <1941239939@qq.com>
2019-04-14 17:25:58 +0800

    生成项目

---
+++ LianDemo/src/main/java/Demo1.java
@@ -0,0 +1,18 @@
+/**
+ * Administrator
+ * 2019/4/14
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+       String x = hello();
+
+       System.out.println(x);
+    }
+    public static String hello(){
+        String li = new String();
+
+        int a = 1+1;
+        return "ming";
+    }
+
+}
